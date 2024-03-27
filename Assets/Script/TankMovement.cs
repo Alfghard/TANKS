dd3e62f499b1eb6e1bf139d3fcef8d1363fb40da
pfarret81 <pierre.farret81@gmail.com>
2024-03-27 21:26:44 +0100

    Tank: déplacement + tir
    Helo tu m’emmeeeerdes

--- Assets/Script/TankMovement.cs
+++ Assets/Script/TankMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class TankMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    [SerializeField] float speed;
+    [SerializeField] private float speed = 5f; // Vitesse de déplacement
+    [SerializeField] private float turnSpeed = 180f; // Vitesse de rotation
 
     void Start()
     {
@@ -14,12 +15,20 @@ public class TankMovement : MonoBehaviour
 
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        rb.velocity = direction * speed;
+        // Mouvement d'avant en arrière
+        float moveAmount = Input.GetAxis("Vertical") * speed;
+        Vector3 move = transform.forward * moveAmount;
+        rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+
+        // Rotation
+        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        Quaternion turn = Quaternion.Euler(0f, turnAmount, 0f);
+        rb.MoveRotation(rb.rotation * turn);
+
+        // Saut (si nécessaire)
         if (Input.GetMouseButtonDown(0))
         {
             rb.AddForce(Vector3.up * 10000);
         }
-
     }
 }
--- Assets/Script/TankShooting.cs
+++ Assets/Script/TankShooting.cs
@@ -8,18 +8,14 @@ public class TankShooting : MonoBehaviour
     [SerializeField] public Transform firePoint;
     [SerializeField] public GameObject missilePrefab;
 
-    void Start()
-    {
-        
-    }
-
+    void Start() { }     
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GameObject missile = Instantiate(missilePrefab, firePoint.position, firePoint.rotation);
-            missile.transform.Rotate(90f, 0, 0);
+            missile.transform.Rotate(180f, 0, 0);
         }
-        
     }
 }
