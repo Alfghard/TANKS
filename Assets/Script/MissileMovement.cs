dafc40e277697d73b4b46ab7acfc1ec35eced9cf
pfarret81 <pierre.farret81@gmail.com>
2024-03-27 23:18:37 +0100

    Implémentation rebond missile basique

--- Assets/Script/MissileMovement.cs
+++ Assets/Script/MissileMovement.cs
@@ -1,14 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MissileMovement : MonoBehaviour
+public class MissileMovementReflect : MonoBehaviour
 {
-    [SerializeField] float speed;
+    public float speed = 10f;
+    private Vector3 currentDirection;
 
-    // Update is called once per frame
+    void Start()
+    {
+        // Initialise la direction du missile pour correspondre à celle du lanceur
+        currentDirection = transform.forward;
+    }
+
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // Déplace le missile dans la direction actuelle
+        transform.Translate(currentDirection * speed * Time.deltaTime, Space.World);
+
+        // Ajuste la rotation du missile pour qu'il "regarde" vers sa direction de mouvement
+        if (currentDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(currentDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, speed * Time.deltaTime * 500);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Calcule le symétrique de la direction actuelle par rapport à la normale de la collision
+        currentDirection = Vector3.Reflect(currentDirection, collision.contacts[0].normal).normalized;
     }
 }
