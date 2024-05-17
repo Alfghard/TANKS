d2a2f79713e594db38b7912463248e801b3910ab
Alfghard <alfghardg@gmail.com>
2024-05-17 10:36:17 +0200

    Problème de souris
    gestion des problème avec la cam

--- Assets/Script/PlayerTank.cs
+++ Assets/Script/PlayerTank.cs
@@ -45,7 +45,9 @@ public class PlayerTank : MonoBehaviour
         if (!paused) {
             TankMovement();     // M�canisme de mouvement de la base du tank
             //TurretMovement_Manette();   // M�canisme de rotation de la tourelle du tank
-            TurretMovement_Souris();        // M�canisme de rotation de la tourelle du tank avec la souris
+            //TurretMovement_Souris();        // M�canisme de rotation de la tourelle du tank avec la souris
+            TurretMovement_Souris2();        // M�canisme de rotation de la tourelle du tank avec la souris
+
             Shoot();            // M�canisme de tir
              // tirer
             if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) )
@@ -129,7 +131,27 @@ public class PlayerTank : MonoBehaviour
     turret.rotation = Quaternion.LookRotation(projection);
     //Debug.Log(direction);
     //Debug.Log(projection);
+    //Debug.Log(turret.position);
     
+    }
+
+    private void TurretMovement_Souris2(){
+        Vector3  mouseInScreen = Input.mousePosition;
+    
+    mouseInScreen.z = mouseInScreen.y;
+    //Centrage
+    mouseInScreen.x += -950;
+    mouseInScreen.z += -500;
+    //Rescale
+    mouseInScreen = mouseInScreen/36;
+    mouseInScreen.y= 1.07F;
+    //tilted
+    mouseInScreen.z = mouseInScreen.z * Mathf.Cos(mouseInScreen.z * Mathf.Deg2Rad);
+    mouseInScreen.z = mouseInScreen.z * 1.5F - 2;
+
+    Vector3 direction = turret.position - mouseInScreen;
+    turret.rotation = Quaternion.LookRotation(direction);
+
     }
 
 
