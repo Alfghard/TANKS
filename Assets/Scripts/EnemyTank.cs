589848051638109c36b2fa765486142576457f0f
pfarret81 <pierre.farret81@gmail.com>
2024-06-04 00:40:17 +0200

    Bug fixed: EnemyTank movement during pause

--- Assets/Scripts/EnemyTank.cs
+++ Assets/Scripts/EnemyTank.cs
@@ -94,8 +94,10 @@ public class EnemyTank : MonoBehaviour
                 Patrol();
             }
         }
-        
-
+        else
+        {
+            agent.velocity = new Vector3(0, 0, 0);
+        }
     }
 
     public void OnCollisionEnter(Collision collision)   // Destruction du tank lors de la collision avec un Missile
